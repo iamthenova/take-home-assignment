626f72e83b332a84bdb755c2b1893cbe05678a49
Ilya Gryaznov <ilya.gryaznov@mvideo.ru>
2025-03-31 11:21:05 +0300

    first commit

--- game/src/main/java/com/cyberspeed/testgame/game/engine/GameResult.java
+++ game/src/main/java/com/cyberspeed/testgame/game/engine/GameResult.java
@@ -1,5 +1,7 @@
 package com.cyberspeed.testgame.game.engine;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
@@ -8,6 +10,7 @@ public record GameResult(
     String[][] matrix,
     BigDecimal reward,
     Map<String, List<String>> appliedWinCombinations,
+    @JsonInclude(Include.NON_NULL)
     String appliedBonusSymbol
 ) {
 
